4eca9d5b4a170f4d8fb38f075dcb2685e00eb995
John-Lindt <54447468+John-Lindt@users.noreply.github.com>
2021-04-30 11:10:33 -0500

    Add files via upload

---
+++ CopyScript.cmd
@@ -0,0 +1,13 @@
+cd C:\Users\lindt\Desktop\GRI_Content
+set /p maxnum=How many entries do you want to make? 
+for i in {1..maxnum}
+do
+echo Question \| 30 Questions in 30 Days Day n \| Smallegan Real Estate^
+Description^
+Listen and Subscribe^
+Facebook Group: https://www.facebook.com/groups/GrandRapidsInformed^
+Instagram: https://www.instagram.com/grinformed/​​^
+Twitter: https://twitter.com/gr_informed/​^
+Have a video request? Looking to buy, sell, or invest in real estate in West Michigan? 
+Email us at info@smalleganrealestate.com^ > GRI_Copy.txt
+done
